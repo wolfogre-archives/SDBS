d85f7a3e5884fa41c71633f5af383124777d4341
Jason Song <i@wolfogre.com>
2016-10-27 16:31:17 +0800

    First commit

---
+++ SDBS/src/main/java/com/wolfogre/sdbs/SdbsApplication.java
@@ -0,0 +1,12 @@
+package com.wolfogre.sdbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdbsApplication.class, args);
+	}
+}
---
+++ UploadData/src/test/java/com/wolfogre/sdbs/UploadDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wolfogre.sdbs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UploadDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
